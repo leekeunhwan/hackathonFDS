f48706c9c19b815e2e4d06d048a26c06175673fa
leekeunhwan <dlrmsghks7@gmail.com>
2018-07-10 18:58:53 +0900

    before merge

--- dist/src.fc61642c.js
+++ dist/src.fc61642c.js
@@ -107,7 +107,7 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 // 메인 페이지 자바스크립트
 
 // 정렬 기준 버튼 관련 로직
-},{}],25:[function(require,module,exports) {
+},{}],9:[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 
@@ -136,7 +136,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = '' || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + '49202' + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + '57469' + '/');
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
 
@@ -277,5 +277,5 @@ function hmrAccept(bundle, id) {
     return hmrAccept(global.parcelRequire, id);
   });
 }
-},{}]},{},[25,67], null)
+},{}]},{},[9,67], null)
 //# sourceMappingURL=/src.fc61642c.map
